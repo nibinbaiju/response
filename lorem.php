ae06ee247748aa415f0b368a3c9aa94146167f9e
nibinbaiju <nibinbaiju@gmail.com>
2021-03-10 15:34:45 +0530

    nextt

---
+++ lorem.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book</title>
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="file:///C:/xampp/htdocs/response/index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="file:///C:/xampp/htdocs/response/index.php">Link</a>
+              </li>
+            
+              
+          </div>
+        </div>
+      </nav>
+ <div class="container">
+     <div class="row">
+         <div class="col col-4 col-sm-4 col-md-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum rerum ducimus sint natus ab ullam voluptatum atque inventore laudantium repellat vitae, aperiam molestias, placeat quia officiis quaerat! Quae, itaque aperiam!</div>
+         <div class="col col-4 col-sm-4 col-md-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque minus accusantium maxime possimus atque dolore facilis illum quidem non, quod, corporis facere impedit, voluptatum ducimus placeat quae! Totam, ducimus esse!
+         <div class="col col-4 col-sm-4 col-md-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, doloribus! Repudiandae, hic! Iste sint aliquid earum voluptate accusamus quibusdam commodi voluptates numquam optio perferendis delectus, quas molestiae odio suscipit modi!</div>
+         </div>
+     </div>
+ </div>
+    
+</body>
+</html>
